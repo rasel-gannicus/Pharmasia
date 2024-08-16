09510a509207e16347bfb87330b4e6f9d8025c88
shafik720 <shafikrasel5@gmail.com>
2024-08-17 02:00:17 +0600

    adding dashboard navbar

--- src/app/(userLayout)/layout.tsx
+++ src/app/(userLayout)/layout.tsx
@@ -1,6 +1,7 @@
 import ReduxProvider from "@/utils/Redux/ReduxProvider";
 import { ubuntu } from "../(commonLayout)/layout";
 import UserDashboardWrapper from "@/components/Wrapper Components/User Dashboard Wrapper/UserDashboardWrapper";
+import UserDashboardNavbar from "@/components/User Dashboard/User Dashboard Navbar/UserDashboardNavbar";
 
 export default async function RootLayout({
   children,
@@ -12,6 +13,7 @@ export default async function RootLayout({
       <html lang="en">
         <body className={ubuntu.className}>
           <div className="min-h-screen mx-auto">
+            <UserDashboardNavbar />
             <UserDashboardWrapper>{children}</UserDashboardWrapper>
           </div>
         </body>
---
+++ src/components/User Dashboard/User Dashboard Navbar/UserDashboardNavbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserDashboardNavbar = () => {
+    return (
+        <div>
+            <h2>User dashboard here</h2>
+        </div>
+    );
+};
+
+export default UserDashboardNavbar;
