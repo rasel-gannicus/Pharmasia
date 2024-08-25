a9f538e9fbc89f17f3952b1628b727baf6c1d647
shafik720 <shafikrasel5@gmail.com>
2024-08-26 05:23:47 +0600

    profile chart shows done

--- src/components/User Dashboard/User Dashboard Profile/ProfileDashboard.tsx
+++ src/components/User Dashboard/User Dashboard Profile/ProfileDashboard.tsx
@@ -3,17 +3,22 @@ import ProfileDashboardCard from "./Profile Dashboard Card/ProfileDashboardCard"
 import ProfileDashboardCard2 from "./Profile Dashboard Card/ProfileDashboardCard2";
 import ProfileDashboardCard3 from "./Profile Dashboard Card/ProfileDashboardCard3";
 import ProfileDashboardCard4 from "./Profile Dashboard Card/ProfileDashboardCard4";
+import ProfileChart from "./Profile Dashboard Chart/ProfileChart";
 
-export const dashboardCardClass = "w-[170px] xl:w-[250px] max-w-[300px] flex flex-col justify-evenly items-center py-5  px-3 bg-white min-h-[300px] rounded text-center "
+export const dashboardCardClass =
+  "w-[170px] xl:w-[250px] max-w-[300px] flex flex-col justify-evenly items-center py-5  px-3 bg-white min-h-[300px] rounded text-center ";
 const ProfileDashboard = () => {
   return (
-    <div className="bg-[#E9EFFB] min-h-screen grid grid-cols-4 gap-5 p-3 ">
+    <div className="bg-[#E9EFFB] min-h-screen grid grid-cols-4 gap-5 py-10 px-5 ">
       <div className=" col-span-3">
-        <div className=" flex justify-center items-center gap-3 ">
+        <div className=" lg:flex justify-between items-center gap-3 ">
           <ProfileDashboardCard />
           <ProfileDashboardCard2 />
           <ProfileDashboardCard3 />
           <ProfileDashboardCard4 />
+        </div>
+        <div className="my-10 w-full bg-white rounded py-10 px-5 ms-auto flex justify-center items-center h-[450px]">
+          <ProfileChart />
         </div>
       </div>
       <div className=" bg-yellow-50"></div>
